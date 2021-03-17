8b45013bec23fca982614f5d453716025b34b2d4
spaceshine <spaceshine909@gmail.com>
2021-03-17 12:39:56 +0300

    added fillVector function and its application

--- forTest.cpp
+++ forTest.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <time.h>
+using namespace std;
+
+vector<int> fillVector(vector<int> forFill)
+{
+    srand(time(0));
+
+    for (int i=0; i < forFill.size(); i++)
+    {
+        forFill[i] = rand();
+        cout << forFill[i] << endl;
+    }
+    return forFill;
+}
 
 int main()
 {
+    vector<int> vectOfInt(10);
+
+    fillVector(vectOfInt);
 
     return 0;
 }
